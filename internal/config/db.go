e99a026a04ba81d6bd4ed7141ff710fc3e0c61ee
Justin <94499968+jzaager@users.noreply.github.com>
2025-01-25 17:57:22 -0500

    Add: Read and write to db config file

---
+++ internal/config/db.go
@@ -0,0 +1,76 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func (c *Config) SetUser(userName string) error {
+	c.CurrentUserName = userName
+	return write(*c)
+}
+
+// read db config from ~/.gatorconfig.json
+func Read() (Config, error) {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, fmt.Errorf("Error reading home dir: %w", err)
+	}
+	fmt.Println(".....")
+	fmt.Println("Reading file at: " + configFilePath)
+	fmt.Println(".....")
+
+	file, err := os.Open(configFilePath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	fullPath := filepath.Join(homeDir, configFileName)
+	return fullPath, nil
+}
+
+func write(cfg Config) error {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(configFilePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/jzaager/gator/internal/config"
+)
+
 func main() {
-	//
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Current config: %+v\n", cfg)
+
+	err = cfg.SetUser("justin")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Current config: %+v\n", cfg)
 }
