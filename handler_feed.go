9da91b733df552f387ce1567bdef7cd070e79b38
Justin <94499968+jzaager@users.noreply.github.com>
2025-01-29 20:34:55 -0500

    Add: listing all feeds

--- handler_feed.go
+++ handler_feed.go
@@ -9,6 +9,31 @@ import (
 	"github.com/jzaager/gator/internal/database"
 )
 
+func handlerListFeeds(s *state, cmd command) error {
+	feeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return fmt.Errorf("Couldn't get feeds: %w", err)
+	}
+
+	if len(feeds) == 0 {
+		fmt.Println("No feeds found")
+		return nil
+	}
+
+	fmt.Printf("Found %d feeds:\n", len(feeds))
+	for _, feed := range feeds {
+		user, err := s.db.GetUserById(context.Background(), feed.UserID)
+		if err != nil {
+			return fmt.Errorf("Couldn't get user: %w", err)
+		}
+
+		printFeed(feed, user)
+		fmt.Println("=========================================================")
+	}
+
+	return nil
+}
+
 func handlerAddFeed(s *state, cmd command) error {
 	if len(cmd.Args) != 2 {
 		return fmt.Errorf("Usage: %s <name> <url>", cmd.Name)
@@ -35,17 +60,18 @@ func handlerAddFeed(s *state, cmd command) error {
 	}
 
 	fmt.Println("Feed created successfully:")
-	printFeed(newFeed)
+	printFeed(newFeed, currentUser)
 	fmt.Println()
-	fmt.Println("==================================================")
+	fmt.Println("=========================================================")
+
 	return nil
 }
 
-func printFeed(feed database.Feed) {
+func printFeed(feed database.Feed, user database.User) {
 	fmt.Printf("* ID:			%s\n", feed.ID)
 	fmt.Printf("* Created:		%v\n", feed.CreatedAt)
 	fmt.Printf("* Updated:		%v\n", feed.UpdatedAt)
 	fmt.Printf("* Name:			%s\n", feed.Name)
 	fmt.Printf("* URL:			%s\n", feed.Url)
-	fmt.Printf("* UserID:		%s\n", feed.UserID)
+	fmt.Printf("* User:			%s\n", user.Name)
 }
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -11,8 +11,12 @@ RETURNING *;
 -- name: GetUser :one
 SELECT * FROM users WHERE name=$1 LIMIT 1;
 
+-- name: GetUserById :one
+SELECT * FROM users WHERE id=$1 LIMIT 1;
+
 -- name: GetUsers :many
 SELECT * FROM users;
 
 -- name: DeleteUsers :exec
 TRUNCATE TABLE users CASCADE;
+
